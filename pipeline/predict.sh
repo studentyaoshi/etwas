d6b5c10830cf640d926518069ab655bae6a74ab3
ys <studentyaoshi@stu.xjtu.edu.cn>
2021-03-17 15:48:51 +0800

    update predict.sh

--- pipeline/predict.sh
+++ pipeline/predict.sh
@@ -2,5 +2,5 @@ tissue=$1
 pheno=$2
 for i in {1..22}
 do
-	Rscript ./FUSION.assoc_test.R --sumstats ../data/gwas/${pheno}.sumstats.gz --weights ../result/$tissue.pos --weights_dir ../result/ --ref_ld_chr ../data/LDREF/hg38/1000G.EUR.hg38. --chr $i --out ../assoc.result/$pheno.$tissue.ldrefhg38.${i} --force_model etwas --perm 10000
+	Rscript ./FUSION.assoc_test.R --sumstats ../data/gwas/${pheno}.sumstats.gz --weights ../result/$tissue.pos --weights_dir ../result/ --ref_ld_chr ../data/LDREF/1000G.EUR.hg38. --chr $i --out ../result/$pheno.$tissue.${i} --force_model etwas --perm 10000
 done
